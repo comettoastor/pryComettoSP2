cd417e39cba39c8d024f03ae511bd97ab4017028
root <mgiraudo@ies21.edu.ar>
2022-09-28 11:42:55 -0300

    bdfdg

--- pryComettoSP2/Properties/Resources.Designer.cs
+++ pryComettoSP2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace pryComettoSP2_PAG_143.Properties {
+namespace pryComettoSP2.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace pryComettoSP2_PAG_143.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("pryComettoSP2_PAG_143.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("pryComettoSP2.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
